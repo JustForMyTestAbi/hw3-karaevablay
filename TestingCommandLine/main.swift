377bed4c543919aaa4e87ea8d32bd684cbf52496
Gray <>
2021-11-30 20:44:07 +0600

    first commit

---
+++ TestingCommandLine/main.swift
@@ -0,0 +1,69 @@
+//
+//  main.swift
+//  TestingCommandLine
+//
+//  Created by ablai erzhanov on 30.11.2021.
+//
+
+import Foundation
+
+//let name = readLine()
+
+//var availableFlights = ["SriLanka, AbuDhabi, Paris"]
+//
+//enum CoronaStatus: String {
+//    case red = "red"
+//    case blue = "blue"
+//    case green = "green"
+//}
+//
+//struct User {
+//    let name: String
+//    let surname: String
+//    let age: Int
+//    let phoneNumber: String
+//    let coronaStatus: CoronaStatus
+//    let preferredCity: String
+//    let preferredHotelRating: String?
+//    let preferredMonth: String
+//}
+//
+//class RegisterToFlight {
+//    let duration: Int = 0
+//    let user: User
+//
+//    init(user: User) {
+//        self.user = user
+//    }
+//
+//    func checkStatus() {
+//        if self.user.coronaStatus != .red {
+//            print("Everything went fine great man")
+//
+//        } else {
+//            print("You need to pass Coronavirus check")
+//        }
+//    }
+//
+//    private func checkFlight() {
+//        showLoading()
+//        if availableFlights.contains(user.preferredCity) {
+//            print("Welcome to the board man")
+//        } else {
+//            print("Unfortunately our company can't fly there")
+//        }
+//    }
+//
+//    private func showLoading() {
+//        print("Loading: 10%")
+//        print("Loading: 20%")
+//        print("Loading: 30%")
+//        print("Loading: 40%")
+//        print("Loading: 50%")
+//        print("Loading: 60%")
+//        print("Loading: 70%")
+//        print("Loading: 80%")
+//        print("Loading: 90%")
+//        print("Loading: 100%")
+//    }
+//}
